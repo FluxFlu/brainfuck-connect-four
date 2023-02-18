7a6b5bee40144dc2fdd053a8fe7fcd90eba499d8
FluxFlu <adamrlukies@gmail.com>
2023-02-17 20:00:58 -0500

    initial commit

---
+++ config.js
@@ -0,0 +1,32 @@
+
+//#########################################################################################//
+//#########################################################################################//
+//#########################################################################################//
+
+const inputStyle = "press";
+// If set to "press," inputs count as pressed only on the first tick they are held.
+// If set to "hold," inputs always count as pressed as long as they are held.
+
+const displayCharacters = true;
+// If set to true, this will log the number of characters used in the program to the console when the program is loaded.
+
+const allowedCommands = "<>+-.,[]#";
+// The commands that can be used by the program.
+
+const pointerConstant = false;
+// If false, the pointer will be reset to 0 every time a new program begins.
+// If true, it will remain constant.
+
+const backgroundColor = "#fff";
+const textColor = "#000";
+// Color of the text and background.
+
+const speed = 1;
+// How many milliseconds to wait in between each loop.
+
+const arrayLength = 80;
+// How many milliseconds to wait in between each loop.
+
+//#########################################################################################//
+//#########################################################################################//
+//#########################################################################################//
